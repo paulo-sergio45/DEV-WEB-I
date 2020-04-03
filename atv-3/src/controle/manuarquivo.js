b147698f550b057a28cdae592c51e7c834c422ac
paulo santos <paulo_sergio2006@hotmail.com.br>
2020-04-03 13:55:50 -0300

    criado lista e tabela falta terminar

---
+++ atv-3/src/controle/manuarquivo.js
@@ -0,0 +1,3 @@
+dataCovid = require("../covid19DataFormata.json")
+
+module.exports = dataCovid;
--- atv-3/src/rotas/rotas.js
+++ atv-3/src/rotas/rotas.js
@@ -1,14 +1,26 @@
 var arqui = require('../controle/manuarquivo');
 var express = require('express');
 
+
+
 var rotas = express.Router();
 
 //depois botar um icone aki para nao dar o error
 rotas.get('/favicon.ico', (req, res) => res.status(204));
 
 rotas.get("/", function(req, res, next) {
-    res.render("./index.ejs");
+
+    res.render("./lista.ejs", { covid: arqui, pg: "lista" });
 });
 
+rotas.get("/lista", function(req, res, next) {
+
+    res.render("./lista.ejs", { covid: arqui, pg: "lista" });
+});
+
+rotas.get("/tabela", function(req, res, next) {
+
+    res.render("./tabela.ejs", { covid: arqui, pg: "tabela" });
+});
 
 module.exports = rotas;
