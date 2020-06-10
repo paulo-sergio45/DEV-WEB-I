372e9c77f3539629d6c5cb21ef38d7874927d520
paulo santos <paulo_sergio2006@hotmail.com.br>
2020-06-10 14:29:07 -0300

    inicio c2

---
+++ atv-8-C2/script/main.js
@@ -0,0 +1,7 @@
+var options
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+
+  });
